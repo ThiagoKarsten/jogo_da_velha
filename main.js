a03edfa026f3d65b4552ccc610b6410f4e46e38e
Thiago Karsten <thiagofk971@gmail.com>
2023-11-15 15:55:45 -0300

    aprimorando o jogo da velha
    ciando arquivo jv02
    add mais scripts ao JavaScript

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+var tabela = ["", "", "", "", "", "", "", "", ""]
 var jogador = "X"
 
-function marcar(td){
-    td.innerHTML = jogador
-
-    if(jogador == "X"){
+function marcar(indice) {
+    tabela[indice] = jogador
+    if (jogador == "X") {
         jogador = "O"
-    }else{
+    } else {
         jogador = "X"
     }
+    render()
 }
+
+function render() {
+    const tds = document.querySelectorAll("td")
+
+    var indice = 0  /* Começa no indice 0 do array */
+    while(indice < tabela.length) {  /* O loop para quando o indice chegar no tamanho da tabela */
+        tds[indice].innerHTML = tabela[indice]
+        indice++ 
+    }
+}
+render()
